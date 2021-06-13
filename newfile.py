ddaf41e05f60ece901d360752c4105f84291493c
S-Vel <85781428+S-Vel@users.noreply.github.com>
2021-06-13 18:19:38 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,55 @@
+print("30 days 30 hour challenge")
+print('30 days 30 hour challenge')
+
+Hours = "thirty"
+print(Hours)
+
+Days = "Thirty days"
+print(Days[0])
+
+Challenge = "I will win"
+print(Challenge[7:10])
+
+Challenge = "I will win"
+print(len(Challenge))
+
+Challenge = "I will win"
+print(Challenge.lower())
+
+a = "30 Days"
+b = "30 hours"
+c = a + b
+print(c)
+
+a = "30 Days"
+b = "30 hours"
+c = a + " " + b
+print(c)
+
+text = "Thirty days and Thirty hours"
+x = text.casefold()
+print(x)
+
+text = "thirty days and thirty hours"
+x = text.casefold()
+print(x)
+
+text = "thirty days and thirty hours"
+x = text.capitalize()
+print(x)
+
+text = "thirty days and thirty hours"
+x = text.isalpha()
+print(x)
+
+text = "thirty days and thirty hours"
+x = text.isalnum()
+print(x)
+
+text = "thirty days and thirty hours"
+x = text.find("a")
+print(x)
+
+text = "thirty days and thirty hours"
+x = text.find("a")
+print(x)
